f5cd81d79fbc62f2300a811353c011478b464195
Kaynaanyare11 <115418165+Kaynaanyare11@users.noreply.github.com>
2023-08-13 11:33:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ export default function app() {
   return (
     <div>
       <h1>Today is our day</h1>
+      <h1>Today is our day</h1>
+      <h1>Today is our day</h1>
+      <h1>Today is our day</h1>
       <h1>We making samething</h1>
 
     </div>
